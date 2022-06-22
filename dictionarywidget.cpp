5e66584e2ca8b05b80d87afaf8eac838948e8974
S.V.I. Vilcrow <svi@vilcrow.net>
2022-06-22 21:25:49 +0300

    A little work.

--- dictionarywidget.cpp
+++ dictionarywidget.cpp
@@ -1,1 +1,44 @@
 #include "dictionarywidget.h"
+
+DictionaryWidget::DictionaryWidget()
+{
+	tableModel = new TableModel();
+	mainLayout = new QVBoxLayout();
+	mainLayout->addWidget(tableModel);
+	createLineEditWidgets();
+	mainLayout->setLayout(gridLayout);
+	createButtons();
+	mainLayout->setLayout(buttonsLayout);
+}
+
+void DictionaryWidget::createLineEditWidgets()
+{
+	originalLabel = new QLabel(tr("Original"));
+	translationLabel = new QLabel(tr("Translation"));
+	statusLabel = new QLabel(tr("Status"));
+	dateLabel = new QLabel(tr("Date"));
+	originalLineEdit = new QLineEdit();
+	translationLineEdit = new QLineEdit();
+	statusLineEdit = new QLineEdit();
+	dateLineEdit = new QLineEdit();
+	gridLayout = new QGridLayout();
+	gridLayout->addWidget(originalLabel, 0, 0, Qt::AlignLeft);
+	gridLayout->addWidget(translationLabel, 0, 1, Qt::AlignLeft);
+	gridLayout->addWidget(statusLabel, 0, 2, Qt::AlignLeft);
+	gridLayout->addWidget(dateLabel, 0, 3, Qt::AlignLeft);
+	gridLayout->addWidget(originalLineEdit, 1, 0, Qt::AlignLeft);
+	gridLayout->addWidget(translationLineEdit, 1, 1, Qt::AlignLeft);
+	gridLayout->addWidget(statusLineEdit, 1, 2, Qt::AlignLeft);
+	gridLayout->addWidget(dateLineEdit, 1, 3, Qt::AlignLeft);
+}
+
+void DictionaryWidget::createButtons()
+{
+	buttonsLayout = new QHBoxLayout();
+	addButton = new QPushButton(tr("Add"));
+	buttonLayout->addWidget(addButton);
+	editButton = new QPushButton(tr("Edit"));
+	buttonLayout->addWidget(editButton);
+	quitButton = new QPushButton(tr("Quit"));
+	buttonLayout->addWidget(quitButton);
+}
--- dictionarywidget.h
+++ dictionarywidget.h
@@ -1,4 +1,29 @@
 #ifndef DICTIONARYWIDGET_H_SENTRY
 #define DICTIONARYWIDGET_H_SENTRY
 
+class DictionaryWidget : public QWidget {
+	Q_OBJECT
+public:
+	DictionaryWidget();
+private:
+	TableModel *tableModel;
+	QLabel *originalLabel;
+	QLabel *translationLabel;
+	QLabel *statusLabel;
+	QLabel *dateLabel;
+	QLineEdit *originalLineEdit;
+	QLineEdit *translationLineEdit;
+	QLineEdit *statusLineEdit;
+	QLineEdit *dateLineEdit;
+	QPushButton *addButton;
+	QPushButton *editButton;
+	QPushButton *deleteButton;
+	QPushButton *quitButton;
+	QVBoxLayout *mainLayout;
+	QGridLayout *gridLayout;
+	QHBoxLayout *buttonsLayout;
+	void createLineEditWidgets();
+	void createButtons();
+};
+
 #endif
